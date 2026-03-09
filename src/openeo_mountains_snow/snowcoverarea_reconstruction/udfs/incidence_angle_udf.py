34657ed2a61b850ba8acb387b4e1728b273a4614
Jeroen Dries <jeroen.dries@vito.be>
2026-03-09 07:59:34 +0100

    add downscaling of shortwave radiation

--- src/openeo_mountains_snow/snowcoverarea_reconstruction/udfs/incidence_angle_udf.py
+++ src/openeo_mountains_snow/snowcoverarea_reconstruction/udfs/incidence_angle_udf.py
@@ -8,13 +8,15 @@ Solar incidence angle calculation UDF.
 
 Computes the angle of incidence of solar radiation on a sloped surface
 using slope, aspect, and solar position angles.
+
+#TODO: pvlib is used  for very minimal math,  consider inlining the code
 """
 
 import xarray
 import pandas as pd
 import pvlib
 import numpy as np
-
+from openeo.udf import inspect
 
 def apply_datacube(cube: xarray.DataArray, context) -> xarray.DataArray:
     """
@@ -27,12 +29,36 @@ def apply_datacube(cube: xarray.DataArray, context) -> xarray.DataArray:
     Returns:
         Solar incidence angle (angle of incidence in degrees)
     """
-    # TODO: Extract slope, aspect, zenith, azimuth from cube
+    slope = cube. sel(bands="slope").values
+    aspect_deg = cube.sel(bands="aspect").values
+    zenith = cube.sel(bands="zenith").values
+    azimuth = cube.sel(bands="azimuth").values
+    agera_ssrd_resampled = cube.sel(bands="solar-radiation-flux").values
+
     # Extract solar angles (placeholder implementation)
     incidence = pvlib.irradiance.aoi(
-        surface_tilt=np.degrees(slope_rad),
-        surface_azimuth=np.degrees(aspect_rad),
-        solar_zenith=solpos['zenith'].values[0],
-        solar_azimuth=solpos['azimuth'].values[0]
+        surface_tilt=slope,
+        surface_azimuth=aspect_deg,
+        solar_zenith=np.degrees(zenith),
+        solar_azimuth=np.degrees(azimuth)
     )
-    return incidence
+
+    inspect(data=incidence.shape, message=f"Incidence shape: {incidence.shape}")
+    inspect(data=incidence, message=f"Incidence: {incidence}")
+
+    #downscale shortwave radiation using incidence angle
+    cosZ = np.cos(zenith)
+    cos_i = np.clip(np.cos(np.radians(incidence)), 0, 1)
+    Qsi = agera_ssrd_resampled * (cos_i / (cosZ + 1e-6))
+
+    slope_band = cube.sel(bands="slope")
+    new_data = xarray.DataArray(
+        Qsi,
+        dims=slope_band.dims,
+        coords={dim: slope_band.coords[dim] for dim in slope_band.dims if dim in slope_band.coords}
+    ).expand_dims(bands=["shortwave-radiation-flux-downscaled"])
+
+    return new_data
+
+    # alternative to return also inputs
+    #return xarray.concat([cube, new_data], dim="bands")
